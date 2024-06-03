cdb47192f910b1c25ff40c812b99694683cacec1
hidge <hidge123@outlook.com>
2024-06-03 23:17:13 +0800

    定义用户模型类

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    mobile = models.CharField(max_length=11, unique=True)
+
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = '用户管理'
+        verbose_name_plural = verbose_name
