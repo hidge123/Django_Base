1c956f82e91d26a3c4ac7805c2fb491aab93d77b
hidge <hidge123@outlook.com>
2024-08-21 23:21:10 +0800

    定义用户地址模型类

--- meiduo_mall/apps/areas/models.py
+++ meiduo_mall/apps/areas/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from utils.models import BaseModel
+from apps.users.models import User
+
 
 # Create your models here.
 class Area(models.Model):
@@ -14,3 +17,24 @@ class Area(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Address(BaseModel):
+    """用户地址"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses', verbose_name='用户')
+    title = models.CharField(max_length=20, verbose_name='地址名称')
+    receiver = models.CharField(max_length=20, verbose_name='收货人')
+    province = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='province_addresses', verbose_name='省')
+    city = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='city_addresses', verbose_name='市')
+    district = models.ForeignKey('areas.Area', on_delete=models.PROTECT, related_name='district_addresses', verbose_name='区')
+    place = models.CharField(max_length=50, verbose_name='地址')
+    mobile = models.CharField(max_length=11, verbose_name='手机')
+    tel = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='固定电话')
+    email = models.CharField(max_length=30, null=True, blank=True, default='', verbose_name='电子邮箱')
+    is_deleted = models.BooleanField(default=False, verbose_name='逻辑删除')
+
+    class Meta:
+        db_table = 'tb_address'
+        verbose_name = '用户地址'
+        verbose_name_plural = verbose_name
+        ordering = ['-update_time']
--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     mobile = models.CharField(max_length=11, unique=True)
     email_active = models.BooleanField(default=False, verbose_name='邮箱验证状态')
+    default_address = models.ForeignKey('areas.Address', related_name='users', null=True, blank=True, on_delete=models.SET_NULL, verbose_name='默认地址')
+
 
     class Meta:
         db_table = 'tb_users'
