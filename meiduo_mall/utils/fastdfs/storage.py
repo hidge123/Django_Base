6bf0a1f6a6c06e753543a165d5edf6c863fc79ea
hidge <hidge123@outlook.com>
2024-11-21 23:46:32 +0800

    haystack对接数据和es

--- meiduo_mall/utils/fastdfs/storage.py
+++ meiduo_mall/utils/fastdfs/storage.py
@@ -1,7 +1,11 @@
 from django.core.files.storage import Storage
+from meiduo_mall.settings import FDFS_BASE_URL
 
 
 class Mystorage(Storage):
+    def __init__(self):
+        self.fdfs_base_url = FDFS_BASE_URL
+
     def _open(self, name, mode="rb"):
         pass
 
@@ -9,4 +13,4 @@ class Mystorage(Storage):
         pass
 
     def url(self, name):
-        return "http://192.168.1.5:8888/" + name
+        return self.fdfs_base_url + name
